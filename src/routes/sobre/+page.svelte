603be98ab1fc61c3464f9bb3bb90baaf25e22ca3
MaviMau <maviaelmelo330@gmail.com>
2024-08-11 19:15:05 -0300

    alterações na página do sobre.
    Foto fake dos integrantes para substituição posterior.

--- src/routes/sobre/+page.svelte
+++ src/routes/sobre/+page.svelte
@@ -17,7 +17,43 @@
 
 <h1>About</h1>
 
-<img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"/>
+<table>
+  <tr>
+    <th>
+      <img class="sobre" src="/images/logoIFPE.png" alt="logo IPFE"  style="width: 20%; height: auto;"/>
+    </th>
+  </tr>
+</table>
+
+<br>
+
+<table>
+  <tr>
+    <th><img class="sobre" src="/images/powerRosa.png" alt="01" style="width: 30%; height: auto;"/></th>
+    <th><img class="sobre" src="/images/powerVermelho.png" alt="02" style="width: 30%; height: auto;"/></th>
+    <th><img class="sobre" src="/images/powerAmarelo.png" alt="03" style="width: 30%; height: auto;"/></th>
+  </tr>
+  <tr>
+    <th><p>Izabele</p></th>
+    <th><p>Rubens Lira</p></th>
+    <th><p>Keila Isabelle</p></th>
+
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th><img class="sobre" src="/images/powerAzul.png" alt="04" style="width: 20%; height: auto;"/></th>
+    <th><img class="sobre" src="/images/powerPreto.png" alt="05" style="width: 20%; height: auto;"/></th>
+  </tr>
+  <tr>
+    <th><p>Dylan Borges</p></th>
+    <th><p>Maviael Melo</p></th>
+
+  </tr>
+</table>
+
+<br><br>
 
 <p>
     Autor: Allan Diego Silva Lima
