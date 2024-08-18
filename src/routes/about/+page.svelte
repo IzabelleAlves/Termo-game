3da7c47f98549ebc1cae0287d38dd3aa4e218234
Izabelle Alves <izabelle.alvesbl@gmail.com>
2024-08-18 11:45:24 -0300

    Final changes made

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -15,60 +15,33 @@
 		<a class="menu" href="/play">Play</a>
 	</nav>
 </header>
+
 <section class="game-instructions">
 	<div class="font1">
-	  <h3>How it works:</h3>
-	  <br>
-	  <p>
+	<h3>How it works:</h3>
+	<br>
+	<p>
 		Find the right word in 6 (six) tries. After each attempt,
 		The tiles show how close you are to the solution.
-	  </p>
-	  <img class="about" src="/images/letraCorreta.png" alt="Exemple 1" />
+	</p>
+	<img class="about" src="/images/letraCorreta.png" alt="Exemple 1" />
 	
-	  <p> The letter <img class="about-icon" src="/images/letraCorretaUnica.png" alt="Exemplo 2" /> is part of the word and is in the correct position. </p>
+	<p> The letter <img class="about-icon" src="/images/letraCorretaUnica.png" alt="Exemplo 2" /> is part of the word and is in the correct position. </p>
 	
-	  <img class="about" src="/images/LetraPosicaoErrada.png" alt="Exemple 3" />
+	<img class="about" src="/images/LetraPosicaoErrada.png" alt="Exemple 3" />
 	
-	  <p> The letter <img class="about-icon" src="/images/LetraPosicaoErradaUnica.png" alt="Exemplo 4" /> is part of the word, but, in other position. </p>
+	<p> The letter <img class="about-icon" src="/images/LetraPosicaoErradaUnica.png" alt="Exemplo 4" /> is part of the word, but, in other position. </p>
 	
-	  <img class="about" src="/images/palavraCompleta.png" alt="Exemple 5" />
-	  <p>If the letter doesn't be green or yellow, it means that it doesn't be part of word.</p>
-	  <br>
-	  <p>Have fun!</p>
+	<img class="about" src="/images/palavraCompleta.png" alt="Exemple 5" />
+	<p>If the letter doesn't be green or yellow, it means that it doesn't be part of word.</p>
+	<br>
+	<p>Have fun!</p>
 	
 	</div>
-	
   </section>
 
-<section class="developers">
-	<h3>Developers:</h3>
-	<div class="developer-grid">
-		<div class="developer">
-			<img class="about" src="/images/Dylan.png" alt="Dylan Borges" />
-			<p>Dylan Borges</p>
-		</div>
-		<div class="developer">
-			<img class="about" src="/images/Izabelle.png" alt="Izabelle Alves" />
-			<p>Izabelle Alves</p>
-		</div>
-		<div class="developer">
-			<img class="about" src="/images/Keila.png" alt="Keila Isabelle" />
-			<p>Keila Isabelle</p>
-		</div>
-		<div class="developer">
-			<img class="about" src="/images/Maviael.png" alt="Maviael Melo" />
-			<p>Maviael Melo</p>
-		</div>
-		<div class="developer">
-			<img class="about" src="/images/Rubens.png" alt="Rubens Lira" />
-			<p>Rubens Lira</p>
-		</div>
+	<div class="button">
+		<a href="/about/aboutUs" class="buttonUnique">Developers</a>
+		<a href="/" class="buttonUnique">Home Page</a>
 	</div>
-</section>
-
-<section class="institution">
-	<h3>Institution:</h3>
-	<img class="about" src="/images/logoIFPE.png" alt="Logo IFPE" />
-</section>
-
 <Footer />
--- src/routes/play/+page.svelte
+++ src/routes/play/+page.svelte
@@ -2,6 +2,8 @@
     import "../../styles/global.css"
     import "../../styles/play.css"
 	import Footer from "../components/footer.svelte";
+    import Keyboard from "../components/gamePage/keyboard.svelte";
+    import TableGame from "../components/gamePage/tableGame.svelte";
 
     let tituloDoJogo: string = "TERMO"
 </script>
@@ -17,87 +19,8 @@
 </header>
 
 <main id="game">
-    <table class="tableGame">
-        <tr>
-            <td class="cell characterWrongPosition">O</td>
-            <td class="cell characterEmpty">D</td>
-            <td class="cell characterEmpty">E</td>
-            <td class="cell characterWrongPosition">I</td>
-            <td class="cell characterEmpty">A</td>
-        </tr>
-        <tr>
-            <td class="cell characterEmpty">C</td>
-            <td class="cell characterWrongPosition">O</td>
-            <td class="cell characterEmpty">U</td>
-            <td class="cell characterEmpty">V</td>
-            <td class="cell characterEmpty">E</td>
-        </tr>
-        <tr>
-            <td class="cell characterWrongPosition">G</td>
-            <td class="cell characterEmpty">R</td>
-            <td class="cell characterWrongPosition">I</td>
-            <td class="cell characterEmpty">L</td>
-            <td class="cell characterCorrect">O</td>
-        </tr>
-        <tr>
-            <td class="cell characterCorrect">P</td>
-            <td class="cell characterCorrect">I</td>
-            <td class="cell characterCorrect">N</td>
-            <td class="cell characterCorrect">G</td>
-            <td class="cell characterCorrect">O</td>
-        </tr>
-        <tr>
-            <td class="cell"></td>
-            <td class="cell"></td>
-            <td class="cell"></td>
-            <td class="cell"></td>
-            <td class="cell"></td>
-        </tr>
-        <tr>
-            <td class="cell"> </td>
-            <td class="cell"> </td>
-            <td class="cell"> </td>
-            <td class="cell"> </td>
-            <td class="cell"> </td>
-        </tr>
-    </table>
-    
-    <aside id="keyboard">
-        <div class="row">
-            <button class="key">Q</button>
-            <button class="key">W</button>
-            <button class="key characterEmpty">E</button>
-            <button class="key characterEmpty">R</button>
-            <button class="key">T</button>
-            <button class="key">Y</button>
-            <button class="key characterEmpty">U</button>
-            <button class="key characterCorrect">I</button>
-            <button class="key characterCorrect">O</button>
-            <button class="key characterCorrect">P</button>
-        </div>
-        <div class="row">
-            <button class="key characterEmpty">A</button>
-            <button class="key">S</button>
-            <button class="key characterEmpty">D</button>
-            <button class="key">F</button>
-            <button class="key characterCorrect">G</button>
-            <button class="key">H</button>
-            <button class="key">J</button>
-            <button class="key">K</button>
-            <button class="key characterEmpty">L</button>
-            <button class="key special">Del</button>
-        </div>
-        <div class="row">
-            <button class="key">Z</button>
-            <button class="key">X</button>
-            <button class="key characterEmpty">C</button>
-            <button class="key characterEmpty">V</button>
-            <button class="key">B</button>
-            <button class="key characterCorrect">N</button>
-            <button class="key">M</button>
-            <button class="key special">Enter</button>
-        </div>
-    </aside>
+    <TableGame />
+    <Keyboard />
 </main>
 
 <Footer />
