a04096c70dfb13da2bcdfe92715c037118a40a42
IsabellleK <keiila_isabelle@outlook.com>
2024-09-10 21:26:28 -0300

    Editando Tipografia e pagina sobre

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -12,44 +12,44 @@
 	<nav class="navigationMenu">
 		<h1 class="menu">TERMO</h1>
 		<a class="menu" href="/">Home Page</a>
-		<a class="menu" href="/play">Play</a>
+		<a class="menu" href="/about">About</a>
 	</nav>
 </header>
 
-<section class="game-instructions">
-	<div class="font1">
-		<h3>How it works:</h3>
-		<br />
-		<p>
-			Find the right word in 6 (six) tries. After each attempt, The tiles show how close you are to
-			the solution.
-		</p>
-		<img class="about" src="/images/letraCorreta.png" alt="Exemple 1" />
-
-		<p>
-			The letter <img class="about-icon" src="/images/letraCorretaUnica.png" alt="Exemplo 2" /> is part
-			of the word and is in the correct position.
-		</p>
-
-		<img class="about" src="/images/LetraPosicaoErrada.png" alt="Exemple 3" />
-
-		<p>
-			The letter <img
-				class="about-icon"
-				src="/images/LetraPosicaoErradaUnica.png"
-				alt="Exemplo 4"
-			/> is part of the word, but, in other position.
-		</p>
-
-		<img class="about" src="/images/palavraCompleta.png" alt="Exemple 5" />
-		<p>If the letter doesn't be green or yellow, it means that it doesn't be part of word.</p>
-		<br />
-		<p>Have fun!</p>
+<section class="developers">
+	
+  <h3>Developers: </h3>
+	<div class="developer-grid">
+		<div class="developer">
+			<img class="aboutUs" src="/images/Dylan.png" alt="Dylan Borges" />
+			<p>Dylan Borges</p>
+		</div>
+		<div class="developer">
+			<img class="aboutUs" src="/images/Izabelle.png" alt="Izabelle Alves" />
+			<p>Izabelle Alves</p>
+		</div>
+		<div class="developer">
+			<img class="aboutUs" src="/images/Keila.png" alt="Keila Isabelle" />
+			<p>Keila Isabelle</p>
+		</div>
+		<div class="developer">
+			<img class="aboutUs" src="/images/Maviael.png" alt="Maviael Melo" />
+			<p>Maviael Melo</p>
+		</div>
+		<div class="developer">
+			<img class="aboutUs" src="/images/Rubens.png" alt="Rubens Lira" />
+			<p>Rubens Lira</p>
+		</div>
 	</div>
 </section>
 
 <div class="button">
-	<a href="/about/aboutUs" class="buttonUnique">Developers</a>
-	<a href="/" class="buttonUnique">Back to home page</a>
+	<a class="buttonUnique" href="/play">Play</a>
 </div>
-<Footer />
+
+<section class="institution">
+	<h3>Institution:</h3>
+	<img class="aboutIf" src="/images/logoIFPE.png" alt="Logo IFPE" />
+</section>
+
+<Footer /> 
